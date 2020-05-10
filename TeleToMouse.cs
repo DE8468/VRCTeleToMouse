47b498dd5c9113137b33968c94caafc198280f08
DE8468 <DE8468@pm.me>
2020-05-10 19:26:30 +0800

    添加项目文件。

---
+++ TeleToMouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace TeleToMouse
+{
+    public class TeleToMouse : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                if (Physics.Raycast(new Ray(Camera.main.transform.position, Camera.main.transform.forward), out RaycastHit raycastHit))
+                {
+                    VRCPlayer.field_Internal_Static_VRCPlayer_0.transform.position = raycastHit.point;
+                }
+            }
+        }
+    }
+}
